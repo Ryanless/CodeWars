211ebd3027e8d5fabe4fed059abf81310a698d9f
Ryanless <denissovm@gmail.com>
2018-05-15 10:10:51 +0200

    Add: kye5 carpark escape

--- kyu5678/kyu5.py
+++ kyu5678/kyu5.py
@@ -1,6 +1,6 @@
+import re
 
-
-# Number of trailing zeros of N!
+#CH: Number of trailing zeros of N!
 
 # Not efficent enough to pass
 def zeros_1(n):
@@ -45,6 +45,182 @@ def isPP(n):
                 return [i, ex]
             ex += 1
     return None
+
+#CH: Letterss of Natac
+
+#times out
+def play_if_enough2(hand, play):
+    #build dict of the resources you have
+    resources = {}
+    for charA in hand:
+        if charA in resources.keys():
+            resources[charA] += 1
+        else:
+            resources[charA] = 1
+    #checks if you have enough resources to build play
+    for charB in play:
+        if (charB in resources) and (resources[charB] > 0):
+            resources[charB] -= 1
+        else:
+            return False, hand
+    #compute your new hand after spending resources
+    newHand = ''
+    for key in resources.keys():
+        newHand += key * resources[key]
+    return True, newHand
+
+
+#works, but can be simplified
+def play_if_enough3(hand, play):
+    new_resources = {}
+    new_hand = ''
+    for char in 'abcdefghijklmnopqrstuvwxyz':
+        new_resources[char] = hand.count(char) - play.count(char)
+        if new_resources[char] < 0:
+            return False, hand
+        else:
+            new_hand += char * new_resources[char]
+    return True, new_hand
+
+def play_if_enough(hand, play):
+    new_hand = ''
+    for char in 'abcdefghijklmnopqrstuvwxyz':
+        new_res = hand.count(char) - play.count(char)
+        if new_res < 0:
+            return False, hand
+        else:
+            new_hand += char * new_res
+    return True, new_hand
+
+#CH: Typoglycemia Generator
+def scramble_words(words):
+    word_array = words.split(" ")
+    result = []
+    for word in word_array:
+        scrambled = scramble_word(word)
+        result.append(scrambled)
+    return ' '.join(result)
+
+
+def scramble_word(word):
+    if len(word) <= 2:
+        return word
+    else:
+
+        start, end = '', ''
+        if word[0] in "-',.":
+            start = word[0]
+            word = word[1:]
+        if word[-1] in "-',.":
+            end = word[-1]
+            word = word[:-1]
+
+        new_word = word[0]
+        shorted_word = word[1: -1]
+        for char in 'abcdefghijklmnopqrstuvwxyz':
+            new_word += char * shorted_word.count(char)
+
+        for index in range(1, len(word) - 1):
+            if word[index] in "-',.":
+                new_word = new_word[:index] + word[index] + new_word[index:]
+
+        return start + new_word + word[-1] + end
+
+
+#CH: Calculate the derivative of a polynomial
+def derivative(poly_deriv):
+    reSplitter = re.compile( r'[\+-]?[^\+-]+')
+    simple_deriv = reSplitter.findall(poly_deriv)
+    result = ''
+    for i in simple_deriv:
+        result += simple_derivative(i)
+    if result == '':
+        return '0'
+    else:
+        return result
+
+
+def simple_derivative(simple_der):
+    reMatcher = re.compile(r'([\+\-])?(\d+)?(x)(\^(\d+))?') # Making the x mandatory makes the derivative of normal numbers
+    matched = reMatcher.match(simple_der)
+    if matched == None:
+        return ''
+    # a * x ^ b --> (a * b) * x ^(b - 1)
+    sign = matched.group(1)
+    if sign == None:
+        sign = ''
+    a  = matched.group(2)
+    if a == None:
+        a = 1
+    else:
+        a = int(a)
+    a_new = a
+    b = matched.group(5)
+    if b != None:
+        b = int(b)
+    elif matched.group(3) != None:
+        b = 1
+    else:
+        b = 0
+    a_new = a * b
+    b_new = b - 1
+    result = ''
+    if a == 0:
+        print
+        return result
+    elif a_new == 1:
+        if b_new == 0:
+            result += sign + '1'
+        else:
+            result += sign
+    else:
+        result += sign + str(a_new)
+    if b_new == 1:
+        result += "x"
+    elif b_new != 0:
+        result += "x^" + str(b_new)
+    return result
+
+
+
+#CH: Car Park Escape
+def escape(carpark):
+    solution = []
+    currPos = None
+    _flagBreak = False
+    for lv in carpark:
+        if _flagBreak: break
+        for i in lv:
+            if i == 2:
+                currPos = [lv.index(i), carpark.index(lv)]
+                _flagBreak = True
+                break
+
+    while currPos[1] != len(carpark) -1:
+        floor = carpark[currPos[1]]
+        for j in range(len(floor)):
+            if floor[j] == 1:
+                diff = j - currPos[0]
+                getSolutionString(diff, solution)
+                currPos[1] += 1
+                currPos[0] = j
+                break
+    diff = len(carpark[currPos[1]]) - 1 - currPos[0]
+    getSolutionString(diff, solution, True)
+
+    return solution
+
+
+def getSolutionString(diff, solution, isLast = False):
+    if diff == 0:
+        if len(solution) == 0 or isLast: return
+        solution[-1] = 'D' + str(int(solution[-1][1:]) + 1)
+    elif diff > 0:
+        solution.append('R' + str(diff))
+        if not isLast: solution.append('D1')
+    else:
+        solution.append('L' + str(abs(diff)))
+        if not isLast: solution.append('D1')
 
 
 
--- test_kyu5678/pseudo_test_kyu5.py
+++ test_kyu5678/pseudo_test_kyu5.py
@@ -7,7 +7,42 @@ def pseudo_test_func(func, args):
     return resultat
 
 
-pseudo_test_func(domain_name, "http://github.com/carbonfive/raygun")
-pseudo_test_func(domain_name, "http://www.zombie-bites.com")
-pseudo_test_func(domain_name, "https://www.cnet.com")
-pseudo_test_func(domain_name, "www.xakep.ru")
+# pseudo_test_func(domain_name, "http://github.com/carbonfive/raygun")
+# pseudo_test_func(domain_name, "http://www.zombie-bites.com")
+# pseudo_test_func(domain_name, "https://www.cnet.com")
+# pseudo_test_func(domain_name, "www.xakep.ru")
+
+# pseudo_test_func(scramble_words, 'professionals')
+# pseudo_test_func(scramble_words, 'i')
+# pseudo_test_func(scramble_words, '')
+# pseudo_test_func(scramble_words, 'me')
+# pseudo_test_func(scramble_words, 'you')
+# pseudo_test_func(scramble_words, 'card-carrying')
+# pseudo_test_func(scramble_words, "shan't")
+# pseudo_test_func(scramble_words, '-dcba')
+# pseudo_test_func(scramble_words, 'I can!')
+
+carpark1 = [[1, 0, 0, 0, 2],
+           [0, 0, 0, 0, 0]]
+
+carpark2 = [[2, 0, 0, 1, 0],
+           [0, 0, 0, 1, 0],
+           [0, 0, 0, 0, 0]]
+
+carpark3 = [[0, 2, 0, 0, 1],
+           [0, 0, 0, 0, 1],
+           [0, 0, 0, 0, 1],
+           [0, 0, 0, 0, 0]]
+
+carpark4 = [[1, 0, 0, 0, 2],
+           [0, 0, 0, 0, 1],
+           [1, 0, 0, 0, 0],
+           [0, 0, 0, 0, 0]]
+
+carpark5 = [[0, 0, 0, 0, 2]]
+
+pseudo_test_func(escape, carpark1)
+pseudo_test_func(escape, carpark2)
+pseudo_test_func(escape, carpark3)
+pseudo_test_func(escape, carpark4)
+pseudo_test_func(escape, carpark5)
