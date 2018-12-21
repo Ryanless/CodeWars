52a21ed28e79c041cb0ff675ed1102359e725d37
Ryanless <denissovm@gmail.com>
2018-12-21 14:16:33 +0100

    Add: kye4

--- kyu1234/kyu4.py
+++ kyu1234/kyu4.py
@@ -35,3 +35,74 @@ def snail(array):
 
 
 
+
+#CH4:Create a funnel
+class Node(object):
+
+    def __init__(self, pos, left, right):
+        self.pos = pos
+        self.left = left
+        self.right = right
+
+
+
+
+class Funnel(object):
+
+    def __init__(self):
+        self._numbers = []
+
+    def fill(self, *args):
+        for n in args:
+            if len(self._numbers) < 15: self._numbers.append(n)
+
+    def drip(self):
+        if len(self._numbers) == 0: return None
+        r = self._numbers[0]
+        #TODO: 2. continue with this kata
+
+
+        return r
+
+    def __str__(self):
+        v = [str(l) for l in self._numbers]
+        v += (15 - len(v)) * " "
+        print(v)
+        rep = "\\{} {} {} {} {}/\n \\{} {} {} {}/\n  \\{} {} {}/\n   \\{} {}/\n    \\{}/"\
+            .format(v[10], v[11], v[12], v[13], v[14], v[6], v[7], v[8], v[9],
+                    v[3], v[4], v[5], v[1], v[2], v[0])
+        return rep
+
+    def weight(self, pos):
+        size = len(self._numbers)
+        if pos >= size: return 0
+        elif pos == 1:
+            dic = {2: 0, 3: 0,
+                   4: 1, 5: 2, 6: 2,
+                   7: 3, 8: 4, 9: 5, 10: 5,
+                   11: 6, 12: 7, 13: 8, 14: 9, 15: 9}
+            return dic[size]
+        elif pos == 2:
+            dic = {3: 0, 4: 0, 5: 1, 6: 2,
+                   7: 2, 8: 3, 9: 4, 10: 5,
+                   11: 5, 12: 6, 13: 7, 14: 8, 15: 9}
+            return dic[size]
+
+        elif pos == 3:
+            dic = {4: 0, 5: 0, 6: 0,
+                   7: 1, 8: 2, 9: 2, 10: 2,
+                   11: 3, 12: 4, 13: 5, 14: 5, 15: 5}
+            return dic[size]
+        elif pos == 4:
+            dic = {5: 0, 6: 0,
+                   7: 0, 8: 1, 9: 2, 10: 2,
+                   11: 2, 12: 3, 13: 4, 14: 5, 15: 5}
+            return dic[size]
+        elif pos == 5:
+            dic = {6: 0,
+                   7: 0, 8: 0, 9: 1, 10: 2,
+                   11: 2, 12: 2, 13: 3, 14: 4, 15: 5}
+            return dic[size]
+
+
+
